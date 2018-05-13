2e9b694966eb02c4c1ca8e35fa11b9d0f12303cc
Marc <marc.l64@outlook.fr>
2018-05-13 14:07:01 +0200

    Define way to choose for point

--- js/script.js
+++ js/script.js
@@ -1,36 +1,65 @@
 let zoneDiv = document.getElementById('zone');
 let circle = document.getElementById('circle');
 let road = document.getElementById('road');
+/**
+* @method chooseWay : Define with numbers which road points will follow
+* @param min number : Minimum number of roads
+* @param max number : Maximum number of roads
+* @return Number which will represente a road
+*/
+function chooseWay(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var choosenWay = chooseWay(1, 2);
 setInterval(move, 10);
+/**
+* @method move : Move points on the road
+* @return Void
+*/
 function move() {
-    let coordinateXPoint = circle.getAttribute('cx');
-    let coordinateYPoint = circle.getAttribute('cy');
-    let coordinateX1Road = road.getAttribute('x1');
-    let coordinateY1Road = road.getAttribute('y1');
-    let coordinateX2Road = road.getAttribute('x2');
-    let coordinateY2Road = road.getAttribute('y2');
-    // let coordinatesPoint = [ coordinateXPoint, coordinateYPoint ];
     // Correct bug : the condition calculate with the number of numerals, not with the number directly
-    coordinateXPoint = Number(coordinateXPoint);
-    coordinateYPoint = Number(coordinateYPoint);
-    coordinateX1Road = Number(coordinateX1Road);
-    coordinateY1Road = Number(coordinateY1Road);
-    coordinateX2Road = Number(coordinateX2Road);
-    coordinateY2Road = Number(coordinateY2Road);
-    if (coordinateXPoint < coordinateX1Road) {
-        coordinateXPoint++;
-        circle.setAttribute('cx', coordinateXPoint);
-    }
-    else if (coordinateXPoint > coordinateX1Road) {
-        coordinateXPoint--;
-        circle.setAttribute('cx', coordinateXPoint);
-    }
-    if (coordinateYPoint < coordinateY1Road) {
-        coordinateYPoint++;
-        circle.setAttribute('cy', coordinateYPoint);
-    }
-    else if (coordinateYPoint > coordinateY1Road) {
-        coordinateYPoint--;
-        circle.setAttribute('cy', coordinateYPoint);
+    let coordinateXPoint = Number(circle.getAttribute('cx'));
+    let coordinateYPoint = Number(circle.getAttribute('cy'));
+    let coordinateX1Road = Number(road.getAttribute('x1'));
+    let coordinateY1Road = Number(road.getAttribute('y1'));
+    let coordinateX2Road = Number(road.getAttribute('x2'));
+    let coordinateY2Road = Number(road.getAttribute('y2'));
+    switch (choosenWay) {
+        case 1:
+            if (coordinateXPoint < coordinateX1Road) {
+                coordinateXPoint++;
+                circle.setAttribute('cx', coordinateXPoint.toString());
+            }
+            else if (coordinateXPoint > coordinateX1Road) {
+                coordinateXPoint--;
+                circle.setAttribute('cx', coordinateXPoint.toString());
+            }
+            if (coordinateYPoint < coordinateY1Road) {
+                coordinateYPoint++;
+                circle.setAttribute('cy', coordinateYPoint.toString());
+            }
+            else if (coordinateYPoint > coordinateY1Road) {
+                coordinateYPoint--;
+                circle.setAttribute('cy', coordinateYPoint.toString());
+            }
+            break;
+        case 2:
+            if (coordinateXPoint < coordinateX2Road) {
+                coordinateXPoint++;
+                circle.setAttribute('cx', coordinateXPoint.toString());
+            }
+            else if (coordinateXPoint > coordinateX2Road) {
+                coordinateXPoint--;
+                circle.setAttribute('cx', coordinateXPoint.toString());
+            }
+            if (coordinateYPoint < coordinateY2Road) {
+                coordinateYPoint++;
+                circle.setAttribute('cy', coordinateYPoint.toString());
+            }
+            else if (coordinateYPoint > coordinateY2Road) {
+                coordinateYPoint--;
+                circle.setAttribute('cy', coordinateYPoint.toString());
+            }
+            break;
     }
 }
--- script.ts
+++ script.ts
@@ -13,38 +13,69 @@ let zoneDiv =  document.getElementById('zone');
 let circle = document.getElementById('circle');
 let road = document.getElementById('road');
 
+
+/**
+* @method chooseWay : Define with numbers which road points will follow
+* @param min number : Minimum number of roads
+* @param max number : Maximum number of roads
+* @return Number which will represente a road
+*/
+function chooseWay(min:number, max:number):number{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var choosenWay = chooseWay(1,2);
+
 setInterval(move, 10);
 
+/**
+* @method move : Move points on the road
+* @return Void
+*/
 function move():void{
-  let coordinateXPoint:number = circle.getAttribute('cx');
-  let coordinateYPoint:number = circle.getAttribute('cy');
-  let coordinateX1Road:number = road.getAttribute('x1');
-  let coordinateY1Road:number = road.getAttribute('y1');
-  let coordinateX2Road:number = road.getAttribute('x2');
-  let coordinateY2Road:number = road.getAttribute('y2');
-  // let coordinatesPoint = [ coordinateXPoint, coordinateYPoint ];
-
   // Correct bug : the condition calculate with the number of numerals, not with the number directly
-  coordinateXPoint = Number(coordinateXPoint);
-  coordinateYPoint = Number(coordinateYPoint);
-  coordinateX1Road = Number(coordinateX1Road);
-  coordinateY1Road = Number(coordinateY1Road);
-  coordinateX2Road = Number(coordinateX2Road);
-  coordinateY2Road = Number(coordinateY2Road);
+  let coordinateXPoint:number = Number(circle.getAttribute('cx'));
+  let coordinateYPoint:number = Number(circle.getAttribute('cy'));
+  let coordinateX1Road:number = Number(road.getAttribute('x1'));
+  let coordinateY1Road:number = Number(road.getAttribute('y1'));
+  let coordinateX2Road:number = Number(road.getAttribute('x2'));
+  let coordinateY2Road:number = Number(road.getAttribute('y2'));
 
-  if(coordinateXPoint < coordinateX1Road){
-    coordinateXPoint++;
-    circle.setAttribute('cx', coordinateXPoint);
-  } else if(coordinateXPoint > coordinateX1Road) {
-    coordinateXPoint--;
-    circle.setAttribute('cx', coordinateXPoint);
-  }
+  switch(choosenWay){
+    case 1 :
+    if(coordinateXPoint < coordinateX1Road){
+      coordinateXPoint++;
+      circle.setAttribute('cx', coordinateXPoint.toString());
+    } else if(coordinateXPoint > coordinateX1Road) {
+      coordinateXPoint--;
+      circle.setAttribute('cx', coordinateXPoint.toString());
+    }
 
-  if(coordinateYPoint < coordinateY1Road){
-    coordinateYPoint++;
-    circle.setAttribute('cy', coordinateYPoint);
-  } else if(coordinateYPoint > coordinateY1Road) {
-    coordinateYPoint--;
-    circle.setAttribute('cy', coordinateYPoint);
+    if(coordinateYPoint < coordinateY1Road){
+      coordinateYPoint++;
+      circle.setAttribute('cy', coordinateYPoint.toString());
+    } else if(coordinateYPoint > coordinateY1Road) {
+      coordinateYPoint--;
+      circle.setAttribute('cy', coordinateYPoint.toString());
+    }
+    break;
+
+    case 2 :
+    if(coordinateXPoint < coordinateX2Road){
+      coordinateXPoint++;
+      circle.setAttribute('cx', coordinateXPoint.toString());
+    } else if(coordinateXPoint > coordinateX2Road) {
+      coordinateXPoint--;
+      circle.setAttribute('cx', coordinateXPoint.toString());
+    }
+
+    if(coordinateYPoint < coordinateY2Road){
+      coordinateYPoint++;
+      circle.setAttribute('cy', coordinateYPoint.toString());
+    } else if(coordinateYPoint > coordinateY2Road) {
+      coordinateYPoint--;
+      circle.setAttribute('cy', coordinateYPoint.toString());
+    }
+    break;
   }
 }
